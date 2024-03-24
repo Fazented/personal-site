f92d8279a0b4b9d737c81a93d2dd25aa18ed096b
Finn Masters <finnjmasters@gmail.com>
2024-03-25 10:20:07 +1300

    Updated work page to be simpler

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,12 @@
 import { defineCollection, z } from 'astro:content';
+import { format } from 'date-fns';
+
 
 export const collections = {
 	work: defineCollection({
 		schema: z.object({
 			title: z.string(),
-			description: z.string(),
-			publishDate: z.coerce.date(),
+            publishDate: z.string().transform((dateString) => format(new Date(dateString), "yyyy-MM-dd'T'HH:mm:ss")),
 			tags: z.array(z.string()),
 			img: z.string(),
 			img_alt: z.string().optional(),
